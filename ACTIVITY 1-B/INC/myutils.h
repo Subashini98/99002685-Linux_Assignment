0e5cae138e936f7807df9427ccff62ee41cca2ec
Subashini98 <69010855+Subashini98@users.noreply.github.com>
2020-10-17 13:08:16 +0530

    Update myutils.h

--- ACTIVITY 1-B/INC/myutils.h
+++ ACTIVITY 1-B/INC/myutils.h
@@ -1,8 +1,7 @@
-#ifndef __MYUTILS_H
-#define __MYUTILS_H
+#include<stdio.h>
     int factorial(int n);
     int isPrime(int a);
     int isPalindrome(int number);
     int vsum(int num,...);
 
-#endif
+
