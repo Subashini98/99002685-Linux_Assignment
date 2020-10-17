b2b92b7b02b9e18202b5a9d404bcfc40a450641b
Subashini98 <69010855+Subashini98@users.noreply.github.com>
2020-10-17 13:00:45 +0530

    Update mystring.h

--- ACTIVITY 1-B/INC/mystring.h
+++ ACTIVITY 1-B/INC/mystring.h
@@ -1,8 +1,6 @@
-#ifndef __MYSTRING_H
-#define __MYSTRING_H
 #include<stdio.h>
   int mystrlen(char str[100]);
   void mystrcpy(char str1[100], char str2[100]);
   void mystrcat(char str1[100], char str2[100]);
   void mystrcmp(char str1[100], char str2[100]);
-#endif
+
