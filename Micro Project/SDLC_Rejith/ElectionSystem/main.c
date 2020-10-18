482dc4e6ead4d533f1d2a5b8b5b3ad20fa0574c1
Subashini98 <69010855+Subashini98@users.noreply.github.com>
2020-10-18 20:25:27 +0530

    Add files via upload

--- Micro Project/SDLC_Rejith/ElectionSystem/main.c
+++ Micro Project/SDLC_Rejith/ElectionSystem/main.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
 
+
+
+int main()
+{
+   int i;
+int choice;
+
+do{
+printf("\n\n ###### Welcome to Election/Voting 2019 #####");
+printf("\n\n 1. Cast the Vote");
+printf("\n 2. Find Vote Count");
+printf("\n 3. Find leading Candidate");
+printf("\n 0. Exit");
+
+printf("\n\n Please enter your choice : ");
+scanf("%d", &choice);
+int a=1;
+switch(choice)
+{
+case 1: castVote(a);break;
+case 2: votesCount(a);break;
+case 3: getLeadingCandidate(a);break;
+default: printf("\n Error: Invalid Choice");
+}
+}while(choice!=0);
+
+//hold the screen
+getchar();
+
+return 0;
+}
