29e51468318b69c4e9788a94c833d9f7f039b641
Subashini98 <69010855+Subashini98@users.noreply.github.com>
2020-10-18 20:32:15 +0530

    Add files via upload

--- Micro Project/SDLC_Rejith/ElectionSystem/inc/election_system.h
+++ Micro Project/SDLC_Rejith/ElectionSystem/inc/election_system.h
@@ -1,1 +1,8 @@
+#ifndef ELECTION_SYSTEM_H_INCLUDED
+#define ELECTION_SYSTEM_H_INCLUDED
 
+int castVote(int);
+int votesCount(int);
+int getLeadingCandidate(int);
+
+#endif // ELECTION_SYSTEM_H_INCLUDED
