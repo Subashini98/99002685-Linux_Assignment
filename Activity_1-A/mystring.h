622d74ed25d2cbb246262613f9ef7d73b023724f
Subashini98 <69010855+Subashini98@users.noreply.github.com>
2020-10-19 19:49:28 +0530

    Update mystring.h

--- Activity_1-A/mystring.h
+++ Activity_1-A/mystring.h
@@ -1,1 +1,8 @@
+#ifndef __MYSTRING_H
+#define __MYSTRING_H
+  int mystrlen(char str[100]);
+  void mystrcpy(char str1[100], char str2[100]);
+  void mystrcat(char str1[100], char str2[100]);
+  void mystrcmp(char str1[100], char str2[100]);
+#endif
 
