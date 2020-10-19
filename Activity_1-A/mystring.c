489237994a21970ea8ca9970bc13e6ae31efdf06
Subashini98 <69010855+Subashini98@users.noreply.github.com>
2020-10-19 20:18:09 +0530

    Update mystring.c

--- Activity_1-A/mystring.c
+++ Activity_1-A/mystring.c
@@ -32,4 +32,23 @@ void mystrcmp(char str1[100], char str2[100])
    else
       printf("str1 = str2\n");
 }
+void mystrcpy(char str1[], char str2[]) // function definition
+{
+    int i = 0;
+    while(str1[i] != '\0')
+    {
+        str2[i] = str1[i];
+        i++;
+    }
+
+    
+    str2[i] =' \0';    
+}
+
+int mystrlen(char str[100]) {
+   int len = 0;
+   while (str[len] != '\0')
+      len++;
+   return (len);
+}
 
