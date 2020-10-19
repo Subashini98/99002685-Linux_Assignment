f572831d463e8d7fb37ecd3f5f0ebc3ad935960c
Subashini98 <69010855+Subashini98@users.noreply.github.com>
2020-10-19 22:21:33 +0530

    Add files via upload

--- Activity_3/String/mystrcpy.c
+++ Activity_3/String/mystrcpy.c
@@ -1,1 +1,13 @@
+#include "mystring.h"
+void mystrcpy(char str1[], char str2[]) // function definition
+{
+    int i = 0;
+    while(str1[i] != '\0')
+    {
+        str2[i] = str1[i];
+        i++;
+    }
 
+    
+    str2[i] =' \0';    
+}
